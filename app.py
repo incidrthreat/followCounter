6b7c981b2cf2960dcd90acb2164a84d54268e2bb
incidrthreat <incidrthreat@gmail.com>
2019-08-23 03:12:15 -0500

    Added flask components anded user to config.json

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-import json, tweepy
+import json, tweepy, string
 
 from flask import Flask
 
+app = Flask(__name__)
 
 
-def init():
-    global api
-    """
-    Get api token and keys from config.json
-    """
+@app.route("/")
+def home():
+    auth()
+    return "{} has {} followers!".format(user.capitalize(), str(get_followers()))
+
+    
+"""
+Get data from config.json
+"""
+def auth():
+    global api, user
     with open('config.json') as config_file:
         data = json.load(config_file)
 
@@ -16,19 +23,18 @@ def init():
     apiSecretKey = data['consumer']['apiSecretKey']
     accessToken = data['tokens']['accessToken']
     accessTokenSecret = data['tokens']['accessTokenSecret']
+    user = data['user']
     
     auth = tweepy.OAuthHandler(apiKey, apiSecretKey)
     auth.set_access_token(accessToken, accessTokenSecret)
     api = tweepy.API(auth)
 
+"""
+Get/return follower count
+"""
 def get_followers():
-    return api.get_user('hackmethod').followers_count
-
-def main():    
-    print(get_followers())
-    
+    return api.get_user(user).followers_count
     
 
 if __name__ == "__main__":
-    init()
-    main()
+    app.run()
