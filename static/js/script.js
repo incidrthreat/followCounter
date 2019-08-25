0ea44cef0c19f8fdaf9cd3b643e167463286c5ee
incidrthreat <incidrthreat@gmail.com>
2019-08-25 05:02:22 -0500

    added tweets and likes event code

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,17 @@
 var source = new EventSource("/update");
-    source.onmessage = function(event) {
-        ////example 1678 followers
-        //var th = Math.floor(event.data/1000); // thousands spot should be 1
-        //var h = Math.floor((event.data/100)%10); // hundreds spot should be 6
-        //var t = Math.floor((event.data/10)%10); // tens spot should be 7
-        //var o = Math.floor(event.data%10); // ones spot should be 8
-        
-        //document.getElementById("thousands").textContent = th;
-        //document.getElementById("hundreds").textContent = h;
-        //document.getElementById("tens").textContent = t;
-        //document.getElementById("ones").textContent = o;
-        
+    source.onmessage = function(event) {   
         odometer.innerHTML = event.data;
+        
+};
+
+var source = new EventSource("/tweets");
+    source.onmessage = function(event) {
+        num_tweets.innerHTML = event.data;
+        
+};
+
+var source = new EventSource("/likes");
+    source.onmessage = function(event) {
+        num_likes.innerHTML = event.data;
         
 };
