f509ddb82f7190392d706ea72694b3140ed8642a
incidrthreat <incidrthreat@gmail.com>
2019-08-25 05:03:25 -0500

    added tweets and likes routes and a some refactor

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import json, tweepy, time
-
 from flask import Flask, render_template, Response, json
 
 app = Flask(__name__)
 
-global user, socketKey
+"""
+Get data from config.json
+"""
+global user
 with open('config.json') as config_file:
         data = json.load(config_file)
 
@@ -14,17 +16,29 @@ accessToken = data['tokens']['accessToken']
 accessTokenSecret = data['tokens']['accessTokenSecret']
 user = data['user']
 
+
+"""
+Defined routes
+"""
 @app.route('/')
 def index():
     return render_template('index.html', value=user.capitalize())
 
 @app.route("/update")
-def send_count():
+def follower_count():
     return Response(get_followers(), mimetype="text/event-stream")
+
+@app.route("/tweets")
+def tweet_count():
+    return Response(get_num_tweets(), mimetype="text/event-stream")
+
+@app.route("/likes")
+def likes_count():
+    return Response(get_num_likes(), mimetype="text/event-stream")
 
     
 """
-Get data from config.json
+Authenticate to twitter
 """
 def auth():   
     global api 
@@ -41,7 +55,31 @@ def get_followers():
             follower_count = str(api.get_user(user).followers_count)
             yield "data: {}\n\n".format(follower_count)
         except tweepy.RateLimitError:
-            print("Rate Limit Error")
+            print("Followers rate limit reached.")
+            time.sleep(15 * 60)
+
+"""
+Get/return number of tweets
+"""
+def get_num_tweets():
+    while True:
+        try:
+            tweet_count = str(api.get_user(user).statuses_count)
+            yield "data: Number of combined tweets/retweets: {}\n\n".format(tweet_count)
+        except tweepy.RateLimitError:
+            print("Tweet count rate limit reached.")
+            time.sleep(15 * 60)
+
+"""
+Get/return number of liked tweets
+"""
+def get_num_likes():
+    while True:
+        try:
+            likes_count = str(api.get_user(user).favourites_count)
+            yield "data: Total Tweets {} has liked: {}\n\n".format(user, likes_count)
+        except tweepy.RateLimitError:
+            print("Likes count rate limit reached.")
             time.sleep(15 * 60)
 
 
